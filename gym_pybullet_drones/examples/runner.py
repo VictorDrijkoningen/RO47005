c3ff603313c672b425c138caffcbf57512558511
VictorDrijkoningen <72612250+VictorDrijkoningen@users.noreply.github.com>
2025-12-21 16:56:20 +0100

    print errors at the end, so they are not missed

--- gym_pybullet_drones/examples/runner.py
+++ gym_pybullet_drones/examples/runner.py
@@ -2,6 +2,8 @@ import json
 from ro47005 import run
 
 
+error_list = list()
+
 if __name__ == "__main__":
     output_list = []
     for seed in range(100):
@@ -12,9 +14,12 @@ if __name__ == "__main__":
             output_list.append(output)
         except Exception as e:
             print(f"error on seed {seed}, exception: {e}")
+            error_list.append(f"error on seed {seed}, exception: {e}")
 
     with open("runner.json", mode="w") as f:
         f.write(json.dumps(output_list))
 else:
     print("ERROR")
 
+
+print(error_list)
